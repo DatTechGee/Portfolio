55cdd34c0898e5ee457695f309a9145c8a36974a
DatTechGee <isaacemmanuel777@gmail.com>
2026-08-30 18:24:32 +0100

    first

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -423,17 +423,6 @@ const HomePage = () => {
             </ScrollReveal>
           </div>
         </div>
-
-        {/* Scroll indicator */}
-        <motion.div
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        >
-          <div className="w-6 h-10 border-2 border-white/20 rounded-full flex justify-center pt-2">
-            <div className="w-1 h-2.5 bg-gold/60 rounded-full" />
-          </div>
-        </motion.div>
       </section>
 
       {/* ===== TECHNOLOGY WALL ===== */}
